6e4a2e10a736995ec1a070d2d194c282dcf70a2c
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2025-12-30 12:05:47 -0800

    added icons and functions to render tweets to html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import { tweetsData } from "./data.js";
 
-console.log(tweetsData)
+const tweetInput = document.getElementById("tweet-input");
+const tweetBtn = document.getElementById("tweet-btn");
+
+tweetBtn.addEventListener("click", function(){
+  const tweetInputValue = tweetInput.value;
+  console.log(tweetInputValue)
+});
+
+function getFeedHtml(){
+  let feedHtml = ""
+  tweetsData.forEach((tweet) => {
+    feedHtml += `
+      <div class="tweet">
+        <div class="tweet-inner">
+            <img src="${tweet.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${tweet.handle}</p>
+                <p class="tweet-text">${tweet.tweetText}</p>
+                <div class="tweet-details">
+                    <span class="tweet-detail">
+                      <i class="fa-regular fa-comment-dots" data-replie=${tweet.uuid}></i>
+                      ${tweet.replies.length}
+                    </span>
+                    <span class="tweet-detail">
+                      <i class="fa-solid fa-heart" data-like=${tweet.uuid}></i>
+                      ${tweet.likes}
+                    </span>
+                    <span class="tweet-detail">
+                      <i class="fa-solid fa-retweet" data-retweet=${tweet.uuid}></i>
+                      ${tweet.retweets}
+                    </span>
+                </div>   
+            </div>            
+        </div>
+      </div>
+    `
+  })
+  return feedHtml;
+}
+
+function renderTweet(){
+  document.getElementById("feed").innerHTML = getFeedHtml()
+}
+
+renderTweet()
